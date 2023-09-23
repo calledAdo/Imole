51ff6c213e27dcb2ac6628f6fb94a0b5f73a1ea7
calledAdo <136274984+calledAdo@users.noreply.github.com>
2023-09-23 23:27:50 +0100

    Update and rename Web3.sol to Imole.sol
    Updated

---
+++ Art/Imole.sol
@@ -0,0 +1,67 @@
+//SPDX-License-Identifier:MIT
+
+pragma solidity ^0.8.18;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "./Sigvalidation.sol"
+
+
+contract Imole {
+    using SigValidation for bytes;
+
+    address _admin;
+    uint _art_Count;
+    address paymentToken;
+
+    struct ART {
+        address _ART_NFT;
+        uint _ART_ROYALTY;
+    }
+
+    mapping(uint => ART) public art_ID;
+
+    constructor(address admin) {
+        _admin = admin;
+    }
+
+    function getART(uint _ID) external view returns (address) {
+        return art_ID[_ID];
+    }
+
+    function addArt(
+        address Art_Nft_Address,
+        uint token_ID,
+        uint royaltyPrice
+    ) external {
+        ERC721 _NFT = ERC721(Art_Nft_Address);
+        require(_NFT.ownerOf(token_ID) == msg.sender);
+        ART storage art = art_ID[_art_Count + 1];
+        art._ART_NFT = Art_Nft_Address;
+        art._ART_ROYALTY = royaltyPrice;
+    }
+
+    function withdrawRevenue(
+        bytes memory _signature,
+        uint _Art_ID,
+        uint amount,
+        address to
+    ) external {
+        require(_signature.isValidSignature(_admin, amount, _Art_ID));
+        ERC20 token = ERC20(paymentToken);
+        token.transfer(to, amount);
+    }
+
+    function Deposit(address depositor, uint amount) external {
+        ERC20 token = ERC20(paymentToken);
+        require(token.transferFrom(msg.sender, address(this), amount));
+
+        emit AccountFunded(depositor, amount);
+    }
+
+    event AccountFunded(address depositor, uint amount);
+    event ArtAdded(address art_Address, uint token_ID, uint royalty_Price);
+}
+
+ 
